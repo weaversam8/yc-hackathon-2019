c1bc5aa4fe56455135acd07c349800db9dd73c47
Sam Weaver <sam@samweaver.com>
2019-11-23 04:43:05 -0800

    Convert txt to yaml hierarchy
    And add YAML parsing. The eventual idea is that we can generate this hierarchy by inspecting the page, perhaps with mozilla/fathom.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # yc-hackathon-2019
+
 YC Hackathon 2019.
+
+## Setup instructions
+
+-   Run python 3
+-   Install `nltk` through `pip`
+-   Install `PyYAML` through `pip`
+-   Run `python` and enter in the interactive prompt the following commands:
+    -   `import nltk`
+    -   `nltk.download('punkt')`
--- index.py
+++ index.py
@@ -1,8 +1,20 @@
+import re
+import yaml
 from nltk.tokenize import sent_tokenize
 
-with open('./policies/text/facebook.txt') as file:
+with open('./policies/text/facebook.yaml') as file:
+    # load the file as one continuous bit of memory
     content = "\n".join(file.readlines())
 
+    tree = yaml.load(content)
+    print(tree)
+    exit()
+
+    # split the file into sentences
     sentences = sent_tokenize(content)
+
+    # replace every consecutive whitespace character with a single space, cleans it up
+    # `list` is needed here because `map` creates a generator :rolling_eyes:
+    sentences = list(map(lambda s: re.sub(r'\s+', ' ', s), sentences))
 
     print(sentences)
