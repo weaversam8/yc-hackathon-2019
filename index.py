5b6073df2055a95717e40d58fb4695c3a33afeac
Sam Weaver <sam@samweaver.com>
2019-11-23 09:05:53 -0800

    Add the ability to generate hierarchical representations of a privacy policy
    Or rather a cleaned up version based on the original YAML (which is manually written in this case but will eventually be automatically generated based on the HTML page structure)

--- index.py
+++ index.py
@@ -1,13 +1,41 @@
 import re
 import yaml
+import json
 from nltk.tokenize import sent_tokenize
+
+# merges adjacent text nodes into one text node in a nested object
+# really annoying and please don't mess with it
+def compact_arrays(tree):
+
+    if isinstance(tree, dict):
+        new_tree = {k: compact_arrays(v) for k, v in tree.items()}
+        return new_tree
+    
+    if isinstance(tree, list):
+        new_tree = []
+        for item in tree:
+            if len(new_tree) == 0:
+                if isinstance(item, str):
+                    new_tree.append(item)
+                else:
+                    new_tree.append(compact_arrays(item))
+            elif isinstance(new_tree[-1], str) and isinstance(item, str):
+                new_tree[-1] += ' ' + item
+            elif isinstance(item, str):
+                new_tree.append(item)
+            else:
+                new_tree.append(compact_arrays(item))
+        return new_tree
+    
+    return tree
 
 with open('./policies/text/facebook.yaml') as file:
     # load the file as one continuous bit of memory
     content = "\n".join(file.readlines())
 
-    tree = yaml.load(content)
-    print(tree)
+    tree = compact_arrays(yaml.load(content))
+
+    print(json.dumps(tree))
     exit()
 
     # split the file into sentences
